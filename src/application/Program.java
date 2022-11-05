e565750d7b7a0889c4e267b8641cc3e0667efb67
Joaomgsa <joaomgsa@gmail.com>
2022-11-05 09:46:35 -0300

    implementando metodo principal

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,59 @@
 package application;
 
+import entities.Company;
+import entities.Individual;
+import entities.TaxPayer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("TaxCalculator");
+
+        Locale.setDefault(Locale.US);
+        List<TaxPayer> list = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number of tax payers");
+        int numberOfTaxPayers = sc.nextInt();
+
+        for (int i = 1; i <= numberOfTaxPayers; i++); {
+            System.out.println("Individual or Company (i/c)?");
+            char typeOfTaxPayers = sc.next().charAt(0);
+            System.out.print("Tax payer #"+ i + "data");
+            if (typeOfTaxPayers == 'i') {
+                System.out.print("Name: ");
+                String name = sc.nextLine();
+                System.out.print("Annual Income: $ ");
+                double annualIncome = sc.nextDouble();
+                System.out.print("Health Expenditures: $ ");
+                double heathExpenditures = sc.nextDouble();
+                list.add(new Individual(name,annualIncome, heathExpenditures));
+
+            }else if (typeOfTaxPayers == 'c') {
+                System.out.print("Name: ");
+                String name = sc.nextLine();
+                System.out.print("Annual Income: $ ");
+                double annualIncome = sc.nextDouble();
+                System.out.print("Number of employees: ");
+                int numberOfEmployees = sc.nextInt();
+                list.add(new Company(name,annualIncome,numberOfEmployees));
+            } else {
+                System.out.print("Type of taxpayer not found, try again");
+            }
+        }
+
+
+        //falta formatar
+        System.out.printf("Summary of Tax payers ");
+        for (TaxPayer tp : list) {
+            System.out.printf("Name: "+ tp.getName());
+            System.out.printf("Name: "+ tp.getName());
+        }
+
+
+        sc.close();
     }
 }
